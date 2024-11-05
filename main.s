d4fef436b7ebc470d987579a9a0b7cb6056d90aa
jx1922 <jx1922@skbl-418-006.ph.ic.ac.uk>
2024-11-05 11:58:18 +0000

    2024/11/5 signal flashing (completed)

--- main.s
+++ main.s
@@ -11,14 +11,18 @@ main:
 setup:	
 	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
+	clrf	PORTC, A	; Clear PORT C to start with all LEDs off
+	movlw	0x00
+	movwf	TRISC, A	; Set PORT C as output for LEDs
 	goto	start
+	
 	; ******* My data and where to put it in RAM *
 myTable:
-	db	'T','h','i','s',' ','i','s',' ','j','u','s','t'
-	db	' ','s','o','m','e',' ','d','a','t','a'
+	db	'a','b','c','d','e','f', 'g','h'
 	myArray EQU 0x400	; Address in RAM for data
 	counter EQU 0x10	; Address of counter variable
-	align	2		; ensure alignment of subsequent instructions 
+	align	2		; ensure alignment of subsequent instructions
+	
 	; ******* Main programme *********************
 start:	
 	lfsr	0, myArray	; Load FSR0 with address in RAM	
@@ -28,14 +32,30 @@ start:
 	movwf	TBLPTRH, A	; load high byte to TBLPTRH
 	movlw	low(myTable)	; address of data in PM
 	movwf	TBLPTRL, A	; load low byte to TBLPTRL
-	movlw	22		; 22 bytes to read
+	movlw	8		; 7 bytes to read
 	movwf 	counter, A	; our counter register
 loop:
         tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
+	movff	TABLAT, PORTC	;Output data to PORT C LEDs
+	call	bigdelay	; Call delay routine
+	call	bigdelay
+	call	bigdelay
+	call	bigdelay
+	call	bigdelay
+	call	bigdelay
+	movff	TABLAT, POSTINC0	;Move read data from TABLAT to FSR0, increment FSR0
 	decfsz	counter, A	; count down to zero
 	bra	loop		; keep going until finished
-	
 	goto	0
 
+	
+	;******Delay Subroutine ********************
+bigdelay:
+	movlw	0x00		; W=0
+Dloop:				;Delay Loop
+	decf	0x08,  f, A	; no carry when 0x00 --> 0xff
+	subwfb  0x10,  f, A	; no carry when 0x00 --> 0xff
+	bc	Dloop		; if carry, hen loop again
+	return			; carry not set so return	
+	
 	end	main
