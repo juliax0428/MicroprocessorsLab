7c655d9c059d9ae3bfa63bfb1f01bbf22dc6928b
jx1922 <jx1922@skbl-418-006.ph.ic.ac.uk>
2024-11-19 12:03:31 +0000

    2024.11.19 main_Keypad(complete) need to add delay

--- main.s
+++ main.s
@@ -1,8 +1,9 @@
 #include <xc.inc>
 
-extrn	Keypad_Setup_1, Keypad_Setup_2, Keypad_loop  ; external subroutines
+extrn	Keypad_Setup, Keypad_Read; external subroutines
 extrn	LCD_Setup, LCD_Write_Message
-	
+extrn	UART_Setup, UART_Transmit_Message
+    
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
@@ -13,25 +14,27 @@ myArray:    ds 0x80 ; reserve 128 bytes for message data
 psect	data    
 	; ******* myTable, data in programme memory, and its length *****
 myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
+	db	'K','e','y','p','a','d',' ',0x0a
 					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
+	myTable_l   EQU  8	; length of data
 	align	2
     
-psect	code, abs	
+psect	code, abs
+	
 rst: 	org 0x0
  	goto	setup
 
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
-	call	Keypad_Setup_1	; setup UART
-	call	Keypad_Setup_2
 	call	LCD_Setup	; setup UART
+	call	UART_Setup
+	call	Keypad_Setup	; setup Keypad
 	goto	start
 	
 	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
+start: 	
+	lfsr	0, myArray	; Load FSR0 with address in RAM	
 	movlw	low highword(myTable)	; address of data in PM
 	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
 	movlw	high(myTable)	; address of data in PM
@@ -40,11 +43,16 @@ start: 	lfsr	0, myArray	; Load FSR0 with address in RAM
 	movwf	TBLPTRL, A		; load low byte to TBLPTRL
 	movlw	myTable_l	; bytes to read
 	movwf 	counter, A		; our counter register
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+loop: 	
+	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
 	decfsz	counter, A		; count down to zero
 	bra	loop		; keep going until finished
 		
+
+	lfsr	2, myArray + 6
+	movwf	INDF2
+	call	Keypad_Read
 	movlw	myTable_l	; output message to UART
 	lfsr	2, myArray
 	call	UART_Transmit_Message
@@ -54,10 +62,12 @@ loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	lfsr	2, myArray
 	call	LCD_Write_Message
 
-	goto	$		; goto current line in code
+	goto	start		; goto current line in code
 
+	
 	; a delay subroutine if you need one, times around loop in delay_count
-delay:	decfsz	delay_count, A	; decrement until zero
+delay:	
+	decfsz	delay_count, A	; decrement until zero
 	bra	delay
 	return
 
