2580cc34bd7510f2e78546b001dc460178807ddb
jx1922 <jx1922@skbl-418-006.ph.ic.ac.uk>
2024-11-21 13:59:16 +0000

    2024/11/21 ACD Multiplications(Incomplete)

--- ACD.s
+++ ACD.s
@@ -2,6 +2,20 @@
 
 global  ADC_Setup, ADC_Read    
     
+psect	udata_acs   ; reserve data space in access ram
+ARG1L: ds    1	    ; reserve 1 byte for ARG1 Low
+ARG1H: ds    1      ; reserve 1 byte for ARG1 High
+ARG1:  ds    1
+ARG2L: ds    1
+ARG2M: ds    1
+ARG2H: ds    1
+RES0:  ds    1
+RES1:  ds    1
+RES2:  ds    1
+RES3:  ds    1
+MSB:   ds    1	    ; Researve 1 byte for Most Significant Byte
+Counter: ds  1	    ; Researve 1 byte for counter
+
 psect	adc_code, class=CODE
     
 ADC_Setup:
@@ -25,88 +39,85 @@ adc_loop:
 	bra	adc_loop
 	return
 ADC_16x16_unsigned:
-	MOVF	ARG1L, W
-	MULWF	ARG2L	    ; ARG1L*ARG2L -- PRODH:PRODL
+	MOVF	ARG1L, W, A
+	MULWF	ARG2L, A	    ; ARG1L*ARG2L -- PRODH:PRODL
 	MOVFF	PRODH, RES1
 	MOVFF	PRODL, RES0
 	
-	MOVF	ARG1H,W
-	MULWF	ARG2H	    ; ARG1L*ARG2H -- PRODH:PRODL
-	MOVF	PRODH, RES3
+	MOVF	ARG1H,W, A
+	MULWF	ARG2H, A    ; ARG1L*ARG2H -- PRODH:PRODL
+	MOVFF	PRODH, RES3
 	MOVFF	PRODL, RES2
 	
-	MOVF	ARG1L, W
-	MULWF	ARG2H
-	MOVF	PRODL, W
-	ADDWF	RES1, F	    ; ADD cross product
-	MOVF	PRODH, W
-	ADDWFC	RES2, F
-	CLRF	WREG
-	ADDWFC	RES3, F
+	MOVF	ARG1L, W, A
+	MULWF	ARG2H, A
+	MOVF	PRODL, W, A
+	ADDWF	RES1, F, A	    ; ADD cross product
+	MOVF	PRODH, W, A
+	ADDWFC	RES2, F,A
+	CLRF	WREG, A
+	ADDWFC	RES3, F, A
 	
-	MOVF	ARG1H, W
-	MULWF	ARG2L	    ; ARG1H*ARG2L -- PRODH:PRODL
-	MOVF	PRODL, W
-	ADDWF	RES1, F	    ; Add Cross Product
-	MOVF	PRODH, W
-	ADDWFC	RES2, F
-	CLRF	WREG
-	ADDWFC	RES3, F
+	MOVF	ARG1H, W, A
+	MULWF	ARG2L, A	    ; ARG1H*ARG2L -- PRODH:PRODL
+	MOVF	PRODL, W, A
+	ADDWF	RES1, F, A	    ; Add Cross Product
+	MOVF	PRODH, W, A
+	ADDWFC	RES2, F, A
+	CLRF	WREG, A
+	ADDWFC	RES3, F, A
 
 ADC_8x24:
-	MOVF	ARG1, W
-	MULWF	ARG2L
-	MOVF	PRODL, RES0
-	MOVF	PRODH, RES1
+	MOVF	ARG1, W, A
+	MULWF	ARG2L, A
+	MOVFF	PRODL, RES0
+	MOVFF	PRODH, RES1
 	
-	MOVF	ARG1, W
-	MULWF	ARG2M
-	MOVF	PRODL, RES1
-	MOVF	PRODH, RES2
+	MOVF	ARG1, W, A
+	MULWF	ARG2M, A
+	MOVFF	PRODL, RES1
+	MOVFF	PRODH, RES2
 	
-	MOVF	ARG1, W
-	MULWF	ARG2H
-	MOVF	PRODL, RES2
-	MOVF	PRODH, RES3
+	MOVF	ARG1, W, A
+	MULWF	ARG2H, A
+	MOVFF	PRODL, RES2
+	MOVFF	PRODH, RES3
 	
 ADC_hex2dec:
-	CLRF    RES1        ; Clear the low byte of the result
-	CLRF    RES2        ; Clear the high byte of the result
-	CLRF    REMAINDER   
-	
-	MOVF    ARG1, W     ; Load lower byte of hex number
-	MOVWF   REMAINDER   ; Store it as the initial remainder
+	CLRF    RES1, A        ; Clear the low byte of the result
+	CLRF    RES2, A        ; Clear the high byte of the result
+	CLRF    RES3, A   
 
     ; First multiplication step: Multiply by k = 0x418A
 	MOVLW   0x8A       ; Load the lower byte of k (0x418A)
-	MOVWF   ARG2L
+	MOVWF   ARG2L, A
 	MOVLW   0x41        ; Load the upper byte of k (0x418A)
-	MOVWF   ARG2H
+	MOVWF   ARG2H, A
 
 	CALL    ADC_16x16_unsigned ; Call the 16x16 multiplication function
-	MOVF    RES3, W     ; Take the most significant byte
-	MOVWF   MSB        ; Store it in MSB register
+	MOVF    RES3, W, A     ; Take the most significant byte
+	MOVWF   MSB, A        ; Store it in MSB register
 	
 	MOVLW   0x03
-	MOVWF   Counter  ; Store loop counter
+	MOVWF   Counter, A  ; Store loop counter
 	
 ADC_Mult_Loop:
 	MOVLW   0x0A        ; Load 0x0A for the next multiplication
-	MOVWF   ARG1
+	MOVWF   ARG1, A
 
-	MOVF    RES0, W        ; Get the lower byte of the result
-	MOVWF   ARG2L      ; Store it as the new remainder
-	MOVF    RES1, W        ; Get the middle byte
-	MOVWF   ARG2M    ; Store it in the next byte of REMAINDER
-	MOVF    RES2, W        ; Get the most significant byte
-	MOVWF   ARG2H    ; Store it in the last byte of REMAINDER
+	MOVF    RES0, W, A        ; Get the lower byte of the result
+	MOVWF   ARG2L, A      ; Store it as the new remainder
+	MOVF    RES1, W, A        ; Get the middle byte
+	MOVWF   ARG2M, A    ; Store it in the next byte of REMAINDER
+	MOVF    RES2, W, A        ; Get the most significant byte
+	MOVWF   ARG2H, A    ; Store it in the last byte of REMAINDER
 	
 	CALL    ADC_8x24    ; Call the 8x24 multiplication function
 	
-	MOVF    RES3, W     ; Take the most significant byte
-	MOVWF   MSB
+	MOVF    RES3, W, A     ; Take the most significant byte
+	MOVWF   MSB, A
 	
-	DECFSZ  Counter, F  ; Decrement the loop counter, if it's zero, exit loop
+	DECFSZ  Counter, F, A  ; Decrement the loop counter, if it's zero, exit loop
 	GOTO    ADC_Mult_Loop      ; Repeat the multiplication step
 
 	return
