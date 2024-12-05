9bfd5ee6fd9a93920bb339bf1b63c3cf9ba2ae86
jx1922 <jx1922@skbl-418-006.ph.ic.ac.uk>
2024-12-05 12:40:02 +0000

    2024/12/05  ultrasonic sensor hc-sr_04 setup and trigger pulse

--- UltrasonicSensors.s
+++ UltrasonicSensors.s
@@ -1,37 +1,63 @@
 #include <xc.inc>
     
     
-extrn	CCP_Echo_Capture
-extrn	motor_setup, Forward, Backward, Left, Right,Stop, Stop, motor_test
+extrn	CCP_Echo_Capture,Echo_Time_H, Echo_Time_L, T1_setup, CCP_setup
 extrn	safety_dist   
 
-global	sensor_setup, sensor_send_signal, sensor_distance
+global	sensor_setup, sensor_trigger, sensor_distance
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Sensor Code for Obstacle Detection                                        ;
+; Setup Sensors and Trigger rountine	                                     ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-psect	ping_code,class=CODE
+psect	sensor_code,class=CODE
     
 sensor_setup:
-    bcf		TRISC, PORTC_RC6_POSN, A    ;RC6 as output for debugging purposes
+    bcf		TRISE, 3,  A
+    bcf		TRISF, 7, A
+    bsf		TRISE, 3, A		    ; set RE6 as trigger 
+    bsf		TRISF, 7, A		    ; Set RE7 as echo
+    clrf	LATE, A
+    clrf	LATF, A
     return          
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Send a short pulse via RE3 to trigger the ultrasonic sensor			;
+;Send a short pulse via RE6 to trigger the ultrasonic sensor			;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-sensor_send_signal:
-    bcf		TRISE, PORTE_RE3_POSN, A    ;RE3 output
-    bcf		PORTE, PORTE_RE3_POSN, A    ;RE3 low
-    bsf		PORTE, PORTE_RE3_POSN, A    ;RE3 high: send trigger pulse
-    nop
-    nop
-    bcf		PORTE, PORTE_RE3_POSN, A    ;RE3 low: end trigger pulse
-    bsf		TRISE, PORTE_RE3_POSN, A    ;RE3 input
+sensor_trigger:
+    bcf		PORTE, 3, A		    ;Trigger is Low
+    call	delay_10us
+    bsf		PORTE, 3, A		    ;Trigger is High
+    call	delay_10us
+    bcf		PORTE, 3, A		    ;Trigger is Low
+    ;bsf	PORTE, PORTE_RE3_POSN, A    ;RE3 high: send trigger pulse
+    ;nop
+    ;nop
+    ;bcf	PORTE, PORTE_RE3_POSN, A    ;RE3 low: end trigger pulse
+    ;bsf	TRISE, PORTE_RE3_POSN, A    ;RE3 input
     retfie
-    
+
 sensor_distance:
-    movf	safety_dist, w, A
-    cpfsgt	CCP_Echo_Capture, A		    ; compare ccp1_count with safety distance
-    return				    ; Z flage is set if distance >= safety_dist
-    
+    ; Check high byte of Echo_Time
+    movf    Echo_Time_H, W, A          ; Load high byte of Timer1 value
+    iorwf   Echo_Time_H, W, A          ; Check if high byte is non-zero
+    btfsc   STATUS, 2, A               ; Skip if Z is set (Echo_Time_H == 0)
+    goto    Distance_Unsafe            ; Unsafe if Echo_Time_H > 0
+
+    ; Compare low byte of Echo_Time with safety_dist
+    movf    Echo_Time_L, W, A          ; Load low byte of Timer1 value
+    subwf   safety_dist, W, A          ; Subtract safety_dist from Echo_Time_L
+    btfsc   STATUS, 0, A                 ; Check carry bit (safety_dist >= Echo_Time_L)
+    goto    Distance_Safe              ; Safe if safety distance >= Echo_Time_L
+
+Distance_Unsafe:
+    bcf    STATUS, 2, A			; Clear Z flag (unsafe)
+    return
+
+Distance_Safe:
+    bsf     STATUS, 2, A		; Set Z flag (safe)
+    return
+
+delay_10us:
+    nop					; Adjust based on clock speed, usually ~4 cycles per NOP
+    RETURN
 end
