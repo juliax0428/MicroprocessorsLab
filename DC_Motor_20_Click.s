5cb7b69c097026cc8b09daf3dffec692873c5caa
jx1922 <jx1922@skbl-418-006.ph.ic.ac.uk>
2024-12-06 11:43:36 +0000

    2024/12/06  change to 1s delay

--- DC_Motor_20_Click.s
+++ DC_Motor_20_Click.s
@@ -1,97 +1,103 @@
 #include <xc.inc>
     
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; RA0=input_1, RD2=Input_2, RG3=input_3, RB4=Input_4			;
-; Motor driver = TC78H651AFNG						;
+;mikrobus1:
+;RA0=input_1, RD2=Input_2, RG3=input_3, RB3=Input_4			;
+;Motor driver = TC78H651AFNG						;
+;mikrobus2:
+;RA1=input_1, RD0=Input_2, RG0=input_3, RB2=Input_4			;
+;Motor driver = TC78H651AFNG						;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    
+
+extrn  delay
+   
 global Forward, Backward, Right, Left, Stop, motor_setup, motor_test
 
-psect	udata_acs
-
-;psect	data
-;R_Input1  EQU 1    ; RC1 -> Right Motor Input 1
-;R_Input2  EQU 2    ; RC2 -> Right Motor Input 2
-;L_Input1  EQU 3    ; RC3 -> Left Motor Input 1
-;L_Input2  EQU 4    ; RC4 -> Left Motor Input 2
-
+    
+psect	udata_acs   ; reserve data space in access ram
 
 psect	motors_code, class=CODE
 
 motor_setup:
-    CLRF PORTC, A        ; Clear PORTC
-    bcf TRISA, 0, A      ; Set RA0 as input 1
-    bcf TRISD, 2, A      ; Set RD2 as input 2
-    bcf TRISG, 3, A      ; Set RG3 as input 3
-    bcf TRISB, 3, A      ; Set RB3 as input 4
-
+    clrf LATA, A         ; Clear LAT registers
+    clrf LATD, A
+    clrf LATG, A
+    clrf LATB, A
+    bcf TRISA, 1, A      ; Set RA1 as output 1
+    bcf TRISD, 0, A      ; Set RD2 as output 2
+    bcf TRISG, 0, A      ; Set RG3 as output 3
+    bcf TRISB, 2, A      ; Set RB3 as output 4
+    return
+    
 Forward:
-    bsf TRISA, 0, A      ; Set RA0 as High
-    bcf TRISD, 2, A      ; Set RD2 as Low
-    bsf TRISG, 3, A      ; Set RG3 as High
-    bcf TRISB, 3, A      ; Set RB3 as Low
-    call Delay			; Delay for specified time
+    bsf PORTA, 1, A      ; Set RA1 as High
+    bcf PORTD, 0, A      ; Set RD0 as Low
+    bsf PORTG, 0, A      ; Set RG0 as High
+    bcf PORTB, 2, A      ; Set RB2 as Low
+    ;call Delay			; Delay for specified time
     return
 
 Backward:
-    bcf TRISA, 0, A      ; Set RA0 as Low
-    bsf TRISD, 2, A      ; Set RD2 as High
-    bcf TRISG, 3, A      ; Set RG3 as Low
-    bsf TRISB, 3, A      ; Set RB3 as High
-    call Delay			; Delay for specified time
+    bcf PORTA, 1, A      ; Set RA0 as Low
+    bsf PORTD, 0, A      ; Set RD2 as High
+    bcf PORTG, 0, A      ; Set RG3 as Low
+    bsf PORTB, 2, A      ; Set RB3 as High
+    ;call Delay			; Delay for specified time
     return
 
 Right:
-    bsf TRISA, 0, A      ; Set RA0 as High
-    bcf TRISD, 2, A      ; Set RD2 as Low
-    bcf TRISG, 3, A      ; Set RG3 as Low
-    bsf TRISB, 3, A      ; Set RB3 as High
-    call Delay			; Delay for specified time
+    bsf PORTA, 1, A      ; Set RA0 as High
+    bcf PORTD, 0, A      ; Set RD2 as Low
+    bcf PORTG, 0, A      ; Set RG3 as Low
+    bsf PORTB, 2, A      ; Set RB3 as High
+    ;call Delay			; Delay for specified time
     return
 
 Left:
-    bcf TRISA, 0, A      ; Set RA0 as Low
-    bsf TRISD, 2, A      ; Set RD2 as High
-    bsf TRISG, 3, A      ; Set RG3 as High
-    bcf TRISB, 3, A      ; Set RB3 as Low
-    call Delay			; Delay for specified time
+    bcf PORTA, 1, A      ; Set RA0 as Low
+    bsf PORTD, 0, A      ; Set RD2 as High
+    bsf PORTG, 0, A      ; Set RG3 as High
+    bcf PORTB, 2, A      ; Set RB3 as Low
+    ;call Delay			; Delay for specified time
     return
 
 Stop:
-    bcf TRISA, 0, A      ; Set all as Low
-    bcf TRISD, 2, A
-    bcf TRISG, 3, A
-    bcf TRISB, 3, A 
-    call Delay			; Delay for specified time
+    bcf PORTA, 1, A      ; Set all as Low
+    bcf PORTD, 0, A
+    bcf PORTG, 0, A
+    bcf PORTB, 2, A 
+    ;call Delay			; Delay for specified time
     return
 
 ; Delay Subroutine (approximately 1 ms delay per call)
-Delay:  
-    MOVLW   0xFF                ; Load WREG with delay count (adjust as needed)
-    MOVWF   0x20, A                ; Store in memory (temp register 0x20)
-Delay_Loop:
-    decfsz  0x20, F, A             ; Decrement delay counter
-    goto    Delay_Loop          ; Repeat until counter reaches zero
-    return                      ; Return from delay
+;Delay:  
+;    MOVLW   0xFF		    ; Load WREG with delay count (adjust as needed)
+;    MOVWF   motor_counter, A        
+;Delay_Loop:
+;    decfsz  motor_counter, F, A             ; Decrement delay counter
+;    goto    Delay_Loop			    ; Repeat until counter reaches zero
+;    return				    ; Return from delay
+
+    
 
 motor_test:
     ; Execute motion sequences
     call Forward
-    call Delay
+    call delay
     call Stop
-    call Delay
+    call delay
     call Backward
-    call Delay
+    call delay
     call Stop
-    call Delay
+    call delay
     call Left
-    call Delay
+    call delay
     call Stop
-    call Delay
+    call delay
     call Right
-    call Delay
+    call delay
     call Stop
-    call Delay
+    call delay
     GOTO motor_test    ; Repeat forever
 
 END 
