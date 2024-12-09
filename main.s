bd868a5ef0f498286fd0df19819ee61d1e88e43b
jx1922 <jx1922@skbl-418-006.ph.ic.ac.uk>
2024-12-09 12:43:47 +0000

    2024/12/09 main(changed)

--- main.s
+++ main.s
@@ -1,11 +1,11 @@
 #include <xc.inc>
 
-extrn	sensor_setup, sensor_trigger, sensor_distance
+extrn	sensor_setup, sensor_trigger, compare_distance
 extrn	motor_setup, Forward, Backward, Left, Right, Stop, motor_test
-extrn	T1_setup, CCP_setup, CCP_reset
+extrn	T1_setup, CCP_setup, CCP_reset, CCP_Interrupt
 extrn	Keypad_Setup, Keypad_Read
 
-global	safety_dist
+global	safety_dist_h, safety_dist_l
     
 psect	udata_acs
 safety_dist_h:	ds 1			; Reserve 1 byte for safety distance high
@@ -31,11 +31,12 @@ setup:
     movwf	safety_dist_l, A	    
     
 main_loop:
-    call	Keypad_Read
-    ;call	sensor_trigger		    ; Send ultrasonic pulse
-    ;call	CCP_reset		    ; Reset CCP and Timer
-    ;call	CCP_Interrupt
-    ;call	compare_distance	    
-    goto	$
+    ;call	Keypad_Read
+    call	motor_test
+    call	sensor_trigger		    ; Send ultrasonic pulse
+    call	CCP_reset		    ; Reset CCP and Timer
+    call	CCP_Interrupt
+    call	compare_distance	    
+    goto	main_loop
     
 end rst
