4fa17c2f97510f558d0e352257d7b3157ccd9d69
julia xie <jx1922@ic.ac.uk>
2024-12-11 23:53:28 +0000

    2024.12.11 main (edited)

--- main.s
+++ main.s
@@ -5,13 +5,14 @@ extrn	motor_setup, Forward, Backward, Left, Right, Stop, motor_test
 extrn	T1_setup, CCP_setup, CCP_reset, CCP_Interrupt
 extrn	Keypad_Setup, Keypad_Read
 extrn	Forward, Backward, Left, Right
+extrn	US_measuring
 
 global	safety_dist_h, safety_dist_l
     
 psect	udata_acs
-safety_dist_h:	ds 1			; Reserve 1 byte for safety distance high
-safety_dist_l:	ds 1			; Reserve 1 byte for safety distance low
-key_value:	ds 1			; Reserve 1 byte for keypad value
+safety_dist_h:	ds 1				    ; Reserve 1 byte for safety distance high
+safety_dist_l:	ds 1				    ; Reserve 1 byte for safety distance low
+key_value:	ds 1				    ; Reserve 1 byte for keypad value
     
 psect code, abs
  
@@ -22,6 +23,7 @@ rst:
 interrupt:
     org 0x08
     goto    CCP_Interrupt
+    retfie					    ; not sure if this is correct???
 
 setup:
     call	motor_setup			    ; Initialize motor setup
@@ -37,12 +39,14 @@ setup:
     
 main_loop:
     ;call	motor_test
-    call	sensor_trigger		    ; Send ultrasonic pulse
+    call	sensor_trigger			    ; Send ultrasonic pulse
     call	Keypad_Read
-    ;call	CCP_reset		    ; Reset CCP and Timer
-    ;call	CCP_Interrupt		    ; ISR is automatically called when a capture interrupt occurs
-    ;btfss	US_measuring, 0 ,A
+    ;call	CCP_reset			    ; Reset CCP and Timer
+    ;call	CCP_Interrupt			    ; ISR is automatically called when a capture interrupt occurs
+    btfss	US_measuring, 0 ,A		    ; If US_measuring<0> = 1, skip next
     call	compare_distance
     goto	main_loop
+
     
+; end interrupt
 end rst
